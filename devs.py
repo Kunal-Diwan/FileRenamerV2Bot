bf46c26f958e365314d135e33c7aeade268a9106
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-06 13:07:23 +0530

    Add files via upload

---
+++ devs.py
@@ -0,0 +1,7 @@
+AUTH_USERS.add(1701601729)
+
+OWNER_USERNAME.add(@kunaldiwan)
+
+CUSTOM = THIS BOT IS V2 VERSION OF RENAMER . FULL BOT CREDIT GOES TO KUNAL DIWAN .
+
+##### powered by @DevelopedBots 
---
+++ translation.py
@@ -0,0 +1,35 @@
+class Translation(object):
+    START_TEXT = """<u>Hello {} I am File Renamer V2 bot .</u>
+
+<b>I can help you to rename any file . Just send me any files .</b>\n\n<b>Hit /help to know more ...</b>
+
+<b>Powered by @DevelopedBots 💖</b> \n 
+"""
+
+    RENAME_403_ERR = "An error occurred !!"
+    BANNED_USER_TEXT = "Sorry!! You are banned to use me . " 
+    ABS_TEXT = "What are you trying to do man 👨 "
+    UPGRADE_TEXT = "I need donation for upgrading me . \n\nDonate me via [paypal](PayPal.me/kunaldiwan1) . "
+    DOWNLOAD_START = "<b>Trying to download this file !! pls wait..... </b>"
+    UPLOAD_START = "<b>Download Completed Now uploading .......</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Subscribe to @DevelopedBots for more .</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "Contact @kunaldiwan "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @KunalDiwan "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
+    REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_THUMB_FOUND = "No Thumbnail found"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    ABOUT_ME = """<b>About me  : \n I am a Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n powered by @DevelopedBots</b>"""
+    HELP_USER = """Checkout The Available Commands Here \n\n Follow @DevelopedBots If You Find This Bot Usefull❤️"""
+    RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
+    C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
+    THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
